06a19756e89534ee7101a747d223ee89fa065b42
nospame <mapson.evan@gmail.com>
2022-03-23 11:39:33 -0700

    add index/show/create actions for employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,24 @@
 class EmployeesController < ApplicationController
+  def index
+    employees = Employee.all
+    render json: employees.as_json
+  end
+
+  def show
+    employee = Employee.find_by(id: params[:id])
+    render json: employee.as_json
+  end
+
+  def create
+    employee = Employee.new(
+      first_name: "Kelly",
+      last_name: "Kapoor",
+      email: "kkapoor@dm.com",
+      phone_number: "555-1234",
+      job_title: "Customer Service",
+      department_id: 3
+    )
+    employee.save
+    render json: employee.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/employees" => "employees#index"
+  get "/employees/:id" => "employees#show"
+  post "/employees" => "employees#create"
 end
