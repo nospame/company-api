e76758a5fa84caafa0a73e33aedd57040b13a08d
nospame <mapson.evan@gmail.com>
2022-03-23 11:52:52 -0700

    add employees update and destroy actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -11,14 +11,32 @@ class EmployeesController < ApplicationController
 
   def create
     employee = Employee.new(
-      first_name: "Kelly",
-      last_name: "Kapoor",
-      email: "kkapoor@dm.com",
-      phone_number: "555-1234",
-      job_title: "Customer Service",
-      department_id: 3
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      job_title: params[:job_title],
+      department_id: params[:department_id]
     )
     employee.save
     render json: employee.as_json
+  end
+
+  def update
+    employee = Employee.find_by(id: params[:id])
+    employee.first_name = params[:first_name] || employee.first_name
+    employee.last_name = params[:last_name] || employee.last_name
+    employee.email = params[:email] || employee.email
+    employee.phone_number = params[:phone_number] || employee.phone_number
+    employee.job_title = params[:job_title] || employee.job_title
+    employee.department_id = params[:department_id] || employee.department_id
+    employee.save
+    render json: employee.as_json
+  end
+
+  def destroy
+    employee = Employee.find_by(id: params[:id])
+    employee.delete
+    render json: {message: "Employee deleted."}
   end
 end
