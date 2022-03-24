53166c2c820eb615348aa9de64ece93fb9139e5e
jeffspatafora <jeffspatafora@gmail.com>
2022-03-24 09:38:05 -0700

    creates Thing model, creates Things controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_181459) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_24_162529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_181459) do
     t.string "phone_number"
     t.string "job_title"
     t.integer "department_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "things", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
